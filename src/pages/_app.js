a765a98f466c5827fe5183aa2d5a565859ae3d87
sranjith95 <droidvictor00@gmail.com>
2026-06-08 12:06:55 +0530

    Fix cloudflare config and app files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,11 @@
 import '@/styles/globals.css';
 import { Toaster } from 'react-hot-toast';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').catch(() => {});
-      });
-    }
-  }, []);
-
   return (
     <>
       <Component {...pageProps} />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-            borderRadius: '12px',
-            padding: '16px',
-          },
-        }}
-      />
+      <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
     </>
   );
 }
